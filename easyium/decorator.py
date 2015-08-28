85791adfd28f1b98a6b43700254dd9cfded54791
Karl Gong <karl.gong@outlook.com>
2015-08-28 21:42:00 +0800

    Fix SupportedBy issue.

--- easyium/decorator.py
+++ easyium/decorator.py
@@ -8,7 +8,10 @@ def SupportedBy(*web_driver_types):
         def handle_args(*args, **kwargs):
             wd_types = []
             for wd_type in web_driver_types:
-                wd_types += wd_type
+                if isinstance(wd_type, list):
+                    wd_types += wd_type
+                else:
+                    wd_types += [wd_type]
 
             current_web_driver_type = args[0].get_web_driver().get_web_driver_type()
             if current_web_driver_type not in wd_types:
