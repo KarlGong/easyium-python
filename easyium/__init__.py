8e211245b786fab5ff8f686b4220f2b0fed2f10e
Karl Gong <karl.gong@outlook.com>
2015-08-31 17:32:31 +0800

    Update easyium/__init__.py

--- easyium/__init__.py
+++ easyium/__init__.py
@@ -1,12 +1,12 @@
+try:
+    import appium
+    appium_installed = True
+except ImportError:
+    appium_installed = False
+
 from .webdriver import WebDriver, WebDriverType
 from .staticelement import StaticElement
 from .identifier import Identifier
 from .waits.waiter import wait_for
 
 __author__ = 'karl.gong'
-
-try:
-    import appium
-    appium_installed = True
-except ImportError:
-    appium_installed = False
