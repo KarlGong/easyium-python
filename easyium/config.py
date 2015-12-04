766c4e1c844ece437f137cf8352570e1279eb17f
Karl Gong <karl.gong@outlook.com>
2015-12-04 10:57:25 +0800

    Reorder the vars.

--- easyium/config.py
+++ easyium/config.py
@@ -9,16 +9,16 @@ class Config:
         # default configuration for web driver
         self.web_driver_page_load_timeout = 30000
         self.web_driver_script_timeout = 30000
-        self.web_driver_pre_wait_time = 0
-        self.web_driver_post_wait_time = 0
         self.web_driver_wait_interval = 1000
         self.web_driver_wait_timeout = 30000
+        self.web_driver_pre_wait_time = 0
+        self.web_driver_post_wait_time = 0
 
         # default configuration for waiter
-        self.waiter_pre_wait_time = 0
-        self.waiter_post_wait_time = 0
         self.waiter_wait_interval = 1000
         self.waiter_wait_timeout = 30000
+        self.waiter_pre_wait_time = 0
+        self.waiter_post_wait_time = 0
 
 
 default_config = Config()
