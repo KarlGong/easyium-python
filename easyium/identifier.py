93b953e3ea2cfd7be2a28693da7728e776f79be2
Karl Gong <karl.gong@outlook.com>
2016-11-25 10:48:00 +0800

    use lambda instead of define method.

--- easyium/identifier.py
+++ easyium/identifier.py
@@ -1,16 +1,8 @@
 class Identifier:
-    @staticmethod
-    def id(element):
-        return "id=" + element.get_attribute("id")
+    id = staticmethod(lambda element: "id=" + element.get_attribute("id"))
 
-    @staticmethod
-    def class_name(element):
-        return "class=" + element.get_attribute("class")
+    class_name = staticmethod(lambda element: "class=" + element.get_attribute("class"))
 
-    @staticmethod
-    def name(element):
-        return "name=" + element.get_attribute("name")
+    name = staticmethod(lambda element: "name=" + element.get_attribute("name"))
 
-    @staticmethod
-    def text(element):
-        return "xpath=.//*[.='%s')]" % element.get_text()
+    text = staticmethod(lambda element: "xpath=.//*[.='%s')]" % element.get_text())
