a04039d5a1f9fb88347f401d61305c84c4eb1122
Karl Gong <karl.gong@outlook.com>
2017-10-11 11:44:53 +0800

    Change element variables from private to protected.

--- examples/google.py
+++ examples/google.py
@@ -5,39 +5,39 @@ from easyium import WebDriver, WebDriverType, StaticElement
 class Google:
     def __init__(self):
         # Create a WebDriver instance for chrome.
-        self.__web_driver = WebDriver(WebDriverType.CHROME)
+        self._web_driver = WebDriver(WebDriverType.CHROME)
 
         # The google apps grid button is in the top-right.
         # This button is always in the page, so it is StaticElement.
-        self.__google_apps_grid_button = StaticElement(self.__web_driver, "class=gb_b")
+        self._google_apps_grid_button = StaticElement(self._web_driver, "class=gb_b")
 
         # After clicked the google apps grid button, the google apps list will be shown.
         # This list is always in the page, although it is invisible, it is StaticElement.
-        self.__google_apps_list = StaticElement(self.__web_driver, "class=gb_ha")
+        self._google_apps_list = StaticElement(self._web_driver, "class=gb_ha")
 
         # Currently the StaticElement does not refer to WebElement in Browser,
         # so open url here is fine.
-        self.__web_driver.open("https://www.google.com")
+        self._web_driver.open("https://www.google.com")
 
     def click_google_apps_grid_button(self):
         # It is StaticElement, easyium will wait it to be visible automatically.
-        self.__google_apps_grid_button.click()
+        self._google_apps_grid_button.click()
 
         # Let's return the GoogleAppsList object.
-        return GoogleAppsList(self.__google_apps_list)
+        return GoogleAppsList(self._google_apps_list)
 
     def quit(self):
-        self.__web_driver.quit()
+        self._web_driver.quit()
 
 
 # This class maps google apps list in the browser.
 class GoogleAppsList:
     def __init__(self, element):
-        self.__element = element
+        self._element = element
 
     def wait_until_ready(self):
         # Wait for the google apps list visible.
-        self.__element.wait_for().visible()
+        self._element.wait_for().visible()
 
         # In most cases we should wait for the mask not existing here .
         # But in this case, no mask here.
@@ -50,21 +50,21 @@ class GoogleAppsList:
         # Find the elements under google apps list.
         # We do not know how many apps in the list, so use find_elements(locator).
         # The found elements are DynamicElements.
-        return [GoogleApp(e) for e in self.__element.find_elements("class=gb_Z")]
+        return [GoogleApp(e) for e in self._element.find_elements("class=gb_Z")]
 
 
 # This class maps google app in the browser.
 class GoogleApp:
     def __init__(self, element):
-        self.__element = element
+        self._element = element
 
         # This locator is relative to parent.
-        self.__name = StaticElement(self.__element, "class=gb_4")
+        self._name = StaticElement(self._element, "class=gb_4")
 
     def get_name(self):
         # get_text() doesn't work here, so use javascript
         # return self.name.get_text()
-        return self.__name.get_web_driver().execute_script('return arguments[0].innerText', self.__name)
+        return self._name.get_web_driver().execute_script('return arguments[0].innerText', self._name)
 
 if __name__ == '__main__':
     google = Google()
